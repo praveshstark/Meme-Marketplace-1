5620f38bc3d09e8967f5508d4a037e20a834e954
Yash Agrawal <43907267+yashagrawal3107@users.noreply.github.com>
2020-11-09 04:59:01 +0530

    Create Meme.sol

---
+++ contracts/Meme.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.5.0;
+
+contact Meme {
+    string public name;
+    uint public memecount = 0;
+    mapping(uint => Meme) public memes;
+
+    struct Meme {
+        uint id;
+        string name;
+        uint price;
+        address payable owner;
+        bool purchased;
+    }
+
+    event MemeCreated(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+    event MemePurchased(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+    constructor() public {
+    name = "Meme Marketplace";
+}
+
+function createMeme(string memory _name, uint _price) public {
+    require(bytes(_name).length >0);
+    require(_price>0);
+    memeCount++;
+    memes[memeCount] = Meme(memecount, _name, _price, msg.sender, false);
+    emit MemeCreated(memecount, _name, _price, msg.sender,false);
+}
+
+function purchasedMeme(uint _id) public payable {
+
+    Meme memory _meme = memes[_id];
+    address payable _seller = _meme.owner;
+    require(_meme.id>0&&_meme.id<= memecount);
+    require(msg.value>= _meme.price);
+    require(!_meme.purchased);
+    require(_seller != msg.sender);
+    _meme.owner = msg.sender;
+    _meme.purchased = true;
+    memes[_id] = _meme;
+    address(_seller).transfer(msg.value);
+    emit memePurchased(memecount, _meme.name,meme.price,msg.sender,true);
+}
+
+}
+
